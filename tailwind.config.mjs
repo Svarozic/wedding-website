790d7aff91109c869e4f7f2fae12af995eade9e2
Peter Petraník <peter.petranik@bitmovin.com>
2023-10-17 19:03:48 +0200

    Custom theme override `cupcake` with base color

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -4,11 +4,23 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 
   daisyui: {
-    themes: ['cupcake'],
+    themes: [
+      {
+        cupcake: {
+          ...require('daisyui/src/theming/themes')['[data-theme=cupcake]'],
+          primary: 'blue',
+          'primary-focus': 'mediumblue',
+          'base-100': '#fffaf0',
+          'base-200': '#faf7f5', // original base-100
+          'base-300': '#efeae6', // original base-200
+        },
+      },
+    ],
     darkTheme: 'cupcake',
     logs: false,
   },
+
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
 };
