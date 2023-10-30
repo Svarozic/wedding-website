6bf25a3328d6e606e85b99328ebee9ebfe6762f6
Peter Petraník <peter.petranik@bitmovin.com>
2023-10-30 21:29:20 +0100

    link astroI18nConfig to middleware

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,6 +1,8 @@
 import {sequence} from "astro/middleware";
 import {useAstroI18n} from "astro-i18n";
 
-const astroI18n = useAstroI18n(undefined /* config */, undefined /* custom formatters */);
+import astroI18nConfig from "../../astro-i18n.config";
+
+const astroI18n = useAstroI18n(astroI18nConfig, undefined /* custom formatters */);
 
 export const onRequest = sequence(astroI18n);
