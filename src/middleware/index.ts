38a069a467557289a585738e1860b4e2aab7a9a0
Peter Petraník <peter.petranik@bitmovin.com>
2023-12-03 19:20:46 +0100

    fix serverless astro-i18n: try .js etension

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,10 +1,7 @@
-import { sequence } from "astro/middleware"
-import { useAstroI18n } from "astro-i18n"
-import astroI18nConfig from "../../astro-i18n.config"
+import {sequence} from "astro/middleware";
+import {useAstroI18n} from "astro-i18n";
+import astroI18nConfig from "../../astro-i18n.config";
 
-const astroI18n = useAstroI18n(
-	astroI18nConfig,
-	undefined /* custom formatters */,
-)
+const astroI18n = useAstroI18n(astroI18nConfig, undefined /* custom formatters */);
 
-export const onRequest = sequence(astroI18n)
+export const onRequest = sequence(astroI18n);
