ac10204ad1f29aa04ef4c118eb60ee720bf9eab3
Peter Petraník <peter.petranik@bitmovin.com>
2024-01-14 17:56:02 +0100

    Revert Serverless try
    Revert "fix serverless astro-i18n: try .js etension"
    
    This reverts commit 38a069a467557289a585738e1860b4e2aab7a9a0.
    
    Revert "fix serverless astro-i18n: import config"
    
    This reverts commit 95c1d5e8d811e82a9854c2c2770eb110b36ddcd4.
    
    Revert "fix serverless astro-i18n config passing and upgrade "astro-i18n": "^2.1.18","
    
    This reverts commit 4cdefcf942b87344be13f25ea8e03e2cd58b4dd5.
    
    Revert "link astroI18nConfig to middleware"
    
    This reverts commit 6bf25a3328d6e606e85b99328ebee9ebfe6762f6.
    
    Revert "redirect index page as  ssr"
    
    This reverts commit dd77135b2a2063723ba74d3404d119eaf73ff7ec.
    
    Revert "# .gitignore .vercel"
    
    This reverts commit e92d0982c6cc3ef51158620391032e51071d1930.
    
    Revert "vercel/serverless"
    
    This reverts commit 4b39115f818490557c8360b1b7298018d058f84c.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   // Enable React to support React JSX components.
   integrations: [react(), tailwind()],
-  output: "server",
-  adapter: vercel(),
 });
